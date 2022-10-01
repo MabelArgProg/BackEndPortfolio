19df6cc69d06dba2a0f44efd8b8a839b061d74ae
MabelArgProg <mabel.cristina9@gmail.com>
2022-10-01 01:38:46 -0300

    Corrigiendo CORS

--- src/main/java/com/portfolio/MCP/controller/CProyecto.java
+++ src/main/java/com/portfolio/MCP/controller/CProyecto.java
@@ -23,7 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendmcp.web.app")
 public class CProyecto {
    
     @Autowired
--- src/main/java/com/portfolio/MCP/controller/controller.java
+++ src/main/java/com/portfolio/MCP/controller/controller.java
@@ -18,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendmcp.web.app")
 public class controller {
 
     @Autowired
